9df9536223d118457a44f22822720fa1bb2b0491
Mario Vaquero <mariovquero@gmail.com>
2026-01-31 14:58:07 -0500

    Timer with full heal
    timer resets on damage but full heals when complete

--- Assets/Features/Health/Scripts/PlayerHealthRegen.cs
+++ Assets/Features/Health/Scripts/PlayerHealthRegen.cs
@@ -7,27 +7,21 @@ public class PlayerHealthRegen : MonoBehaviour
 
     public float timer;
     public float maxTime;
-
     public Health playerHealth;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
     void FixedUpdate()
     {
         timer += Time.fixedDeltaTime;
         if (timer >= maxTime)
         {
+            Debug.Log("Full Heal");
             playerHealth.fullHeal();
-            timer = 0;
+            timer = 0;  
         }
+    }
+
+    public void resetTimer()
+    {
+        timer = 0;
     }
 }
