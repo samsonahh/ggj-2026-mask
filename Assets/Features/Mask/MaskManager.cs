d4bc3a7bc88214d4f565850d99b570a3a387d970
Samson Wu <78169553+samsonahh@users.noreply.github.com>
2026-02-01 13:21:35 -0500

    Fixed mask texture bug

--- Assets/Features/Mask/MaskManager.cs
+++ Assets/Features/Mask/MaskManager.cs
@@ -8,12 +8,12 @@ using UnityEngine.UI;
 public class MaskManager : MonoBehaviour
 {
     [System.Serializable]
-    public class TextureSpritePair
+    public class MaterialSpritePair
     {
-        public Texture Texture;
+        public Material Material;
         public Sprite Sprite;
     }
-    [SerializeField] private List<TextureSpritePair> _maskTextureSpritePairs = new();
+    [SerializeField] private List<MaterialSpritePair> _maskTextureSpritePairs = new();
     [SerializeField, Required] private Image _playerOneMaskImage;
     [SerializeField, Required] private Image _playerTwoMaskImage;
     [SerializeField, Required] private Renderer _playerOneMaskRenderer;
@@ -21,13 +21,13 @@ public class MaskManager : MonoBehaviour
     [SerializeField, Required] private PlayerModelFlasher _playerOneModelFlasher;
     [SerializeField, Required] private PlayerModelFlasher _playerTwoModelFlasher;
 
-    private TextureSpritePair _firstMaskTextureSpritePair;
-    private TextureSpritePair _secondMaskTextureSpritePair;
+    private MaterialSpritePair _firstMaskMaterialSpritePair;
+    private MaterialSpritePair _secondMaskMaterialSpritePair;
     
     private void Start()
     {
-        _firstMaskTextureSpritePair = _maskTextureSpritePairs.RandomElement();
-        _secondMaskTextureSpritePair = _maskTextureSpritePairs.RandomElement();
+        _firstMaskMaterialSpritePair = _maskTextureSpritePairs.RandomElement();
+        _secondMaskMaterialSpritePair = _maskTextureSpritePairs.RandomElement();
         ApplyMasks();
         
         _playerOneModelFlasher.OnMaterialsReset += ApplyMasks;
@@ -42,9 +42,9 @@ public class MaskManager : MonoBehaviour
 
     public void ApplyMasks()
     {
-        _playerOneMaskImage.sprite = _firstMaskTextureSpritePair.Sprite;
-        _playerTwoMaskImage.sprite = _secondMaskTextureSpritePair.Sprite;
-        _playerOneMaskRenderer.material.mainTexture = _firstMaskTextureSpritePair.Texture;
-        _playerTwoMaskRenderer.material.mainTexture = _secondMaskTextureSpritePair.Texture;
+        _playerOneMaskImage.sprite = _firstMaskMaterialSpritePair.Sprite;
+        _playerTwoMaskImage.sprite = _secondMaskMaterialSpritePair.Sprite;
+        _playerOneMaskRenderer.sharedMaterial = _firstMaskMaterialSpritePair.Material;
+        _playerTwoMaskRenderer.sharedMaterial = _secondMaskMaterialSpritePair.Material;
     }
 }
