397c4ac74d7a10b7a58886c69e319f1ce016d33a
Samson Wu <78169553+samsonahh@users.noreply.github.com>
2026-02-01 00:02:20 -0500

    Mash sfx

--- Assets/Features/Player/Scripts/MashMeter/MashMeter.cs
+++ Assets/Features/Player/Scripts/MashMeter/MashMeter.cs
@@ -1,19 +1,18 @@
-﻿using Animancer;
+﻿using System;
+using Animancer;
 using Sirenix.OdinInspector;
 using UnityEngine;
 using UnityEngine.Events;
+using Random = UnityEngine.Random;
 using UnityEvent = UnityEngine.Events.UnityEvent;
 
 public class MashMeter : MonoBehaviour
 {
     [field: SerializeField] public float MaxMashMeterValue { get; private set; } = 100f;
     [field: SerializeField, ReadOnly] public float MashMeterValue { get; private set; }
-
-    [SerializeField] private float _ripperMashStrength = 1.1f;
-    [SerializeField] private float _victimMashStrength = 1f;
     
-    //[SerializeField] private StringAsset _stretchSfxName;
-    private Transform _audioInstance;
+    [SerializeField] private float _mashStrength = 1f;
+    [SerializeField] private float _ripperPassiveMashSpeed = 0.25f;
 
     public enum Ripper
     {
@@ -24,12 +23,19 @@ public class MashMeter : MonoBehaviour
     [field: SerializeField] public UnityEvent OnMashStarted { get; private set; } = new();
     [field: SerializeField] public UnityEvent<Ripper> OnWin { get; private set; } = new();
     
+    [Header("Mash")]
+    [SerializeField] private StringAsset _stretchOneShotSfxName;
+    [SerializeField] private StringAsset _ripWinSfxName;
+    [SerializeField] private StringAsset _ripFailSfxName;
+    
     [Header("On Finish Juice")]
     [SerializeField] private float _cameraShakeDuration = 2f;
     [SerializeField] private float _cameraShakeAmplitude = 10f;
     [SerializeField] private float _cameraShakeFrequency = 10f;
     [SerializeField] private float _hitStopTimeScale = 0.01f;
     [SerializeField] private float _hitStopDuration = 0.3f;
+
+    private bool _isMashing;
 
     private void Awake()
     {
@@ -43,12 +49,28 @@ public class MashMeter : MonoBehaviour
 
     public void StartMash()
     {
+        _isMashing = true;
         OnMashStarted.Invoke();
+    }
+
+    private void Update()
+    {
+        if (!_isMashing)
+            return;
+        
+        // ripper has meter slowly creep towards their side
+        MashMeterValue = Mathf.Clamp(MashMeterValue - _ripperPassiveMashSpeed * Time.deltaTime, 0f, MaxMashMeterValue);
+        if(MashMeterValue <= 0f)
+            Win(Ripper.Ripper);
+        else if(MashMeterValue >= MaxMashMeterValue)
+            Win(Ripper.Victim);
     }
 
     public void AddRipperMash()
     {
-        MashMeterValue = Mathf.Clamp(MashMeterValue - _ripperMashStrength, 0f, MaxMashMeterValue);
+        MashMeterValue = Mathf.Clamp(MashMeterValue - _mashStrength, 0f, MaxMashMeterValue);
+        
+        OnMeterMashed();
         
         if(MashMeterValue <= 0f)
             Win(Ripper.Ripper);
@@ -56,14 +78,25 @@ public class MashMeter : MonoBehaviour
 
     public void AddVictimMash()
     {
-        MashMeterValue = Mathf.Clamp(MashMeterValue + _victimMashStrength, 0f, MaxMashMeterValue);
+        MashMeterValue = Mathf.Clamp(MashMeterValue + _mashStrength, 0f, MaxMashMeterValue);
+
+        OnMeterMashed();
         
         if(MashMeterValue >= MaxMashMeterValue)
             Win(Ripper.Victim);
     }
 
+    private void OnMeterMashed()
+    {
+        AudioManager.Instance.Play(_stretchOneShotSfxName, AudioManager.MixerTarget.SFX, null, Random.Range(0.8f, 1.2f));
+    }
+
     private void Win(Ripper winner)
     {
+        _isMashing = false;
+        
+        AudioManager.Instance.Play(winner == Ripper.Ripper ? _ripWinSfxName : _ripFailSfxName, AudioManager.MixerTarget.SFX);
+        
         TimeScaleManager.Instance.StartImpactFrames(_hitStopTimeScale, _hitStopDuration);
         CameraShaker.Instance.ShakeCamera(_cameraShakeAmplitude, _cameraShakeFrequency, _cameraShakeDuration);
         OnWin?.Invoke(winner);
